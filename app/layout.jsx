3a06085eecde232efd0553b933c1fa1a8eaab3c2
omargpx <omargpax.dev@gmail.com>
2025-04-17 19:53:08 -0500

    ref:seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,10 +32,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SEO />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jetbrainsMono.variable} antialiased pt-[80px] md:pt-[120px] xl:pt-[150px]`}
       >
-        <SEO />
         <Header/>
         {children}
         <Analytics />
