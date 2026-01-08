717869d3e29a675716dd8e1ccfb528c29c4d1577
omargpax <omargpax.dev@gmail.com>
2026-01-07 21:43:06 -0500

    fix:update: cv-titles

--- app/page.jsx
+++ app/page.jsx
@@ -57,7 +57,7 @@ const Home = () => {
             {/* social */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                href="/omargpax_cv.pdf"
+                href="/omargpax_cv_EN_.pdf"
                 download
                 className="cursor-pointer"
                 aria-label="Download CV"
